f5fba48520274f545abc99b21633003034a005c7
h13i32maru <h13i32maru@gmail.com>
2013-07-20 16:23:17 +0900

    作業途中

--- src/ninstaller.js
+++ src/ninstaller.js
@@ -30,6 +30,15 @@
     return fBound;
   };
 
+  var arrayToMap = function(array, key) {
+    var result = {};
+    for (var i = 0; i < array.length; i++) {
+      var val = array[i];
+      result[val[key]] = val;
+    }
+    return result;
+  };
+
   global.nInstaller = {
     /**
      * 指定されたmanifestを読み込んでリソースのインストールを行う.
@@ -53,8 +62,70 @@
      */
     _initSuccess: function(xhr, callback) {
       var manifestText = xhr.responseText;
-      var manifest = JSON.parse(manifestText);
-      console.log(manifest);
+      var newManifest = JSON.parse(manifestText);
+
+      this._getCurrentManifest(bind(function(currentManifest) {
+        var resources = this._getNewResource(newManifest, currentManifest);
+        this._fetchResources(resources, bind(this._saveResouces, this));
+      }, this));
+      //this.updateResource(manifest);
+    },
+
+    _getNewResource: function(newManifest, currentManifest) {
+      var resources;
+      if (!currentManifest) {
+        resources = [].concat(newManifest.js.resources);
+      } else {
+        resources = [];
+        var currentResourcesMap = arrayToMap(currentManifest.js.resources, 'path');
+        var newResources = newManifest.js.resouces;
+        for (var i = 0; i < newResources.length; i++) {
+          var newResource = newResources[i];
+          var currentResource = currentResourcesMap[newResource.path];
+          if (newResource.md5 !== currentResource.md5) {
+            resources.push(newResource);
+          }
+        }
+      }
+
+      return resources;
+    },
+
+    _getCurrentManifest: function(callback) {
+      var db = openDatabase('ninstaller', "0.1", "nInstaller", 5 * 1000 * 1000);
+      db.transaction(function(transaction){
+        transaction.executeSql('CREATE TABLE IF NOT EXISTS manifest (manifest TEXT)');
+        transaction.executeSql('SELECT * from manifest', null, function(transaction, result){
+          if (result.rows.length < 1) {
+            callback(null);
+          } else {
+            var row =  result.rows.item(0);
+            callback(JSON.parse(row.manifest));
+          }
+        });
+      }, function(error){
+      });
+    },
+
+    updateResource: function(manifest) {
+      var db = openDatabase('ninstaller', "0.1", "nInstaller", 5 * 1000 * 1000);
+      db.transaction(function(tr){
+
+
+
+        tr.executeSql('CREATE TABLE IF NOT EXISTS js (id integer PRIMARY KEY AUTOINCREMENT, path TEXT, md5 TEXT, time TEXT)', []);
+        tr.executeSql('INSERT INTO js (path, md5, time) VALUES (?, ?, ?)', ['hoge', 'foo', 'bar']);
+
+        /*
+        tr.executeSql("SELECT * from js", [], function(){
+          console.log(arguments);
+        });
+        */
+      }, function(error) {
+        console.log(error);
+      }, function(){
+        console.log('success');
+      });
     }
   };
 
